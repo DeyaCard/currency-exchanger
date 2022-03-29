241cc2933ec9969e31f8db9fe1e40c1872c0bafb
Deya Card <thedeyacard@gmail.com>
2022-03-29 15:18:30 -0700

    add getElements details

--- src/js/currency.js
+++ src/js/currency.js
@@ -2,12 +2,12 @@ export default class Currency {
   static async currencyEx(amount, currencyTo) {
     try {
       const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currencyTo}`)
-      console.log(response);
-
+      
       if (!response.ok) {
         throw Error (response.statusText);
       }
-      return response.json();
+      const jsonData = await response.json();
+      return jsonData;
     } catch(error) {
       return error.message;
     }
--- src/js/index.js
+++ src/js/index.js
@@ -4,15 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './../css/styles.css';
 import Currency from './currency.js'
 
+console.log(Currency)
+
 function clearFields() {
   $('.showErrors').text(""); 
   $('.showExchange').text("");
 }
 
-function getElements(response) {
+function getElements(response, amount) {
     if(response) {
-      const amount = (response.usdAmount);
-      const convertTo = (response.usdConversion);
+      console.log(amount)
+      const convertTo = (response.conversion_rate);
       $('.showExchange').append(`The exchange rate is: ${convertTo}`);
     } else {
       $('.showErrors').text(`There was an error: ${response.message}`);
@@ -21,12 +23,14 @@ function getElements(response) {
 
 
 $(document).ready(function() {
-  $('#exchangeCurrency').click(function() {
+  $('#exchangeForm').submit(function(event) {
+    event.preventDefault();
     let amount = $('#amount').val();
+    let currencyTo = $('#currencyTo').val();
     clearFields();
-    currencyExchange.currencyEx(amount, currencyTo) 
+    Currency.currencyEx(amount, currencyTo) 
       .then(function(response) {
-        getElements(response, currencyTo);
+        getElements(response, amount);
       });
   });
 }); 
